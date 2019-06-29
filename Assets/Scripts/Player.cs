900d2b404d306240935384f1d7efc5c76cf4266a
patrickeddy <p@patrickeddy.com>
2019-06-29 13:02:41 -0700

    Lock/unlock cursor with P.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -20,11 +20,16 @@ public class Player : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Cursor.lockState = Cursor.lockState == CursorLockMode.None ? CursorLockMode.Locked : CursorLockMode.None;
+            Cursor.visible = Cursor.lockState == CursorLockMode.None;
+        }
 	}
 }
--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -18,13 +18,17 @@ public class PlayerCamera : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
-		md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
-		smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
-		smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
-		mouseLook += smoothV;
+        // only activate camera movement if locked on window
+        if (Cursor.lockState == CursorLockMode.Locked)
+        {
+		    var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+		    md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+		    smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+		    smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
+		    mouseLook += smoothV;
 
-		transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
-		character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+		    transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+		    character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+        }
 	}
 }
